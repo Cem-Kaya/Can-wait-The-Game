4cca19a6829234cfc7a9bd8c548fe3b9a341837e
bahamert <bersoy@sabanciuniv.edu>
2022-07-18 22:08:35 +0300

    my_commit

--- Rand_test/Game_Prototype_0/Assets/scripts/box_mover.cs
+++ Rand_test/Game_Prototype_0/Assets/scripts/box_mover.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class box_mover : MonoBehaviour
 {
     public float speed;
-    Rigidbody2D rigidbody ; 
+    Rigidbody2D rb ; 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //uwu here we goooooooo
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //i will never pay
     }
 }
