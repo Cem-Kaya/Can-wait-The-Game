fdc4a2009c75e742907eb9ba2553ed0492998f27
bahamert <bersoy@sabanciuniv.edu>
2022-07-27 11:15:11 +0300

    created camera singleton

---
+++ Rand_test/Game_Prototype_0/Assets/scripts/Camera_controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_controller : MonoBehaviour
+{
+
+    public static Camera_controller instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
