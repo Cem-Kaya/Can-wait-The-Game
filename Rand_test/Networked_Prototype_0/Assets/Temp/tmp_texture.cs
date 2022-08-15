774930460cc164d43615e154d056fbcad716b272
cem-kaya <cemkaya2704@gmail.com>
2022-08-15 15:06:07 +0300

    ading wfc visualization

--- Rand_test/Networked_Prototype_0/Assets/Temp/tmp_texture.cs
+++ Rand_test/Networked_Prototype_0/Assets/Temp/tmp_texture.cs
@@ -5,31 +5,50 @@ using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 
-
 public class tmp_texture : MonoBehaviour    
 {
+	Color light_grey = new Color(0.7f, 0.7f, 0.7f, 1f);
+	Color dark = new Color( 0.05f, 0.05f, 0.05f, 1f);
+
+
+
+	float grid_size;
+	int line_thickness;
+	[SerializeField] Texture2D texture;
+
+	private void Awake()
+	{
+		texture = new Texture2D(1000, 1000, TextureFormat.ARGB32, false);
+		line_thickness = 6;
+	}
 	// Start is called before the first frame update
 	void Start()
 	{
-		// Create a new 2x2 texture ARGB32 (32 bit with alpha) and no mipmaps
-		Texture2D texture = new Texture2D(1080, 1080, TextureFormat.ARGB32, false);
+		grid_size= 10;
 
-		// set the pixel values
-		for(int x = 0;x < texture.width; x++)
+		for (int x = 0;x < texture.width; x++)
 		{
 			for (int y = 0; y < texture.height; y++)
 			{
-				if (x % 30 <3 )
+				if (x % (texture.width / grid_size)  < 1 * line_thickness)
 				{
-					texture.SetPixel(x, y, new Color(0.0f, 0.0f, 0.0f, 1f));
+					texture.SetPixel(x, y, light_grey);
+				}		
+				else if (y % (texture.height / grid_size)  < 1 * line_thickness)
+				{
+					texture.SetPixel(x, y, light_grey);
 				}
-				else if(y % 30 < 3)
+				else if (x > texture.width - line_thickness)
 				{
-					texture.SetPixel(x, y, new Color(0.0f, 0.0f, 0.0f, 1f));
+					texture.SetPixel(x, y, light_grey);
+				}
+				else if (y > texture.height - line_thickness)
+				{
+					texture.SetPixel(x, y, light_grey);
 				}
 				else
 				{
-					texture.SetPixel(x, y, new Color(1.0f, 1.0f, 1.0f, 1f));
+					texture.SetPixel(x, y, dark ); 
 				}
 			}
 		}
