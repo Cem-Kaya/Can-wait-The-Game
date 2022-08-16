297b68130acddfcd492652d2ec43b7e9998def9b
cem-kaya <cemkaya2704@gmail.com>
2022-08-16 16:42:26 +0300

    adding large look up tabels

--- Rand_test/Networked_Prototype_0/Assets/Temp/tmp_texture.cs
+++ Rand_test/Networked_Prototype_0/Assets/Temp/tmp_texture.cs
@@ -6,17 +6,146 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
+public enum door_dir
+{
+	blank,
+	u,
+	r,
+	d,
+	l,
+	ur,
+	rd,
+	dl,
+	lu,
+	rl,
+	ud,
+	urd,
+	rdl,
+	dlu,
+	lur,
+	urdl
+}
+
 public class Tile {
-	bool collapsed = false;
-	List<int> possibles = new List<int>{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };	
-	public int possible_num{
-		get{ return possibles.Count; }
+	public Tile() {
+		collapsed = false;
+		possibles  = new List<door_dir> { door_dir.blank,
+													door_dir.u,
+													door_dir.r,
+													door_dir.d,
+													door_dir.l,
+													door_dir.ur,
+													door_dir.rd,
+													door_dir.dl,
+													door_dir.lu,
+													door_dir.rl,
+													door_dir.ud,
+													door_dir.urd,
+													door_dir.rdl,
+													door_dir.dlu,
+													door_dir.lur,
+													door_dir.urdl };
+		value = door_dir.blank;
+	}
+	public bool collapsed ;
+	List<door_dir> possibles;
+    public int possible_num {get{ return possibles.Count; }	}
+	door_dir value;
+	public bool try_collapse()
+	{
+		if (possible_num > 0)
+		{
+			value = possibles [ Random.Range(0, possible_num ) ];
+			collapsed = true;
+		}
+		return false;
+	}
+	
+	public void propogate ( List<door_dir> removables )
+	{
+		foreach(door_dir d in removables)
+		{
+			possibles.Remove(d);
+		}
+    }
+}
+public class Floor
+{
+	int max_x, max_y;
+
+	Dictionary<door_dir, (int, int)> tile_map_coord = new Dictionary<door_dir, (int, int)>();
+	
+	Dictionary<(int, int), Tile> floor_data = new Dictionary<(int, int), Tile>();
+
+	Dictionary<(door_dir , string), List<door_dir>> rules = new Dictionary<(door_dir, string), List<door_dir>> (); 
+	public void fill_tables()
+	{
+		rules.Add((door_dir.blank, "u"), new List<door_dir> {door_dir.d, door_dir.urdl, door_dir.rdl, door_dir.dl, door_dir.ud, door_dir.dlu, door_dir.rd, door_dir.urd } );
+        rules.Add((door_dir.blank, "r"), new List<door_dir> { door_dir.l, door_dir.urdl, door_dir.rdl, door_dir.dl, door_dir.lu, door_dir.dlu, door_dir.rl, door_dir.lur });
+        rules.Add((door_dir.blank, "d"), new List<door_dir> { door_dir.u, door_dir.urdl, door_dir.url, door_dir.lu, door_dir.ud, door_dir.dlu, door_dir.ur, door_dir.urd });
+        rules.Add((door_dir.blank, "l"), new List<door_dir> { door_dir.r, door_dir.urdl, door_dir.rdl, door_dir.rl, door_dir.ur, door_dir.lur, door_dir.rd, door_dir.urd });
+
+        rules.Add((door_dir.u, "u"), new List<door_dir> { door_dir.r, door_dir.u, door_dir.l, door_dir.ur, door_dir.rl, door_dir.blank, door_dir.lu, door_dir.lur });
+        rules.Add((door_dir.u, "r"), new List<door_dir> { door_dir.r, door_dir.u, door_dir.l, door_dir.ur, door_dir.rl, door_dir.blank, door_dir.lu, door_dir.lur });
+
+        //initialize tile_map_coord
+        tile_map_coord.Add(door_dir.blank, (3, 0));
+		tile_map_coord.Add(door_dir.u, (3, 1));
+		tile_map_coord.Add(door_dir.r, (1, 0));
+		tile_map_coord.Add(door_dir.d, (2, 0));
+		tile_map_coord.Add(door_dir.l, (0, 0));
+		tile_map_coord.Add(door_dir.ur, (2, 2));
+		tile_map_coord.Add(door_dir.rd, (3, 2));
+		tile_map_coord.Add(door_dir.dl, (0, 1));
+		tile_map_coord.Add(door_dir.lu, (1, 2));
+		tile_map_coord.Add(door_dir.rl, (2, 1));
+		tile_map_coord.Add(door_dir.ud, (1, 1));
+		tile_map_coord.Add(door_dir.urd, (2, 3));
+		tile_map_coord.Add(door_dir.rdl, (3, 3));
+		tile_map_coord.Add(door_dir.dlu, (0, 2));
+		tile_map_coord.Add(door_dir.lur, (1, 3));
+		tile_map_coord.Add(door_dir.urdl, (0, 3));
+
+
+
+	}
+	public Floor()
+	{
+		fill_tables();
+
+		max_x = 10;
+		max_y = 10;
+		for (int i = 0; i < max_x; i++)
+		{
+			for (int j = 0; j < max_y; j++)
+			{
+				floor_data[(i, j)] = new Tile();
+			}
+		}
+	}
+
+
+	public Tile get_min_enthropy()
+	{
+		Tile ret = null ;
+		int current_min = int.MaxValue ;
+		foreach (var item in floor_data)
+		{
+			if(! item.Value.collapsed && item.Value.possible_num < current_min)
+			{
+				ret = item.Value;
+			}
+		}
+		return ret;
+	}
+	public Start_collapse()
+	{
+
 	}
 }
 
 public class tmp_texture : MonoBehaviour    
 {
-	Dictionary<(int, int), Tile> floor = new Dictionary<(int,int), Tile>();
 
 
 	Color light_grey = new Color(0.7f, 0.7f, 0.7f, 1f);
