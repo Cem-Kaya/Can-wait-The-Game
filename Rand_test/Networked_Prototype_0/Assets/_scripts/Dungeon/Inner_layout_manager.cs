5db00732596551c6f7684b3d4c43c0ad0b433315
cem-kaya <cemkaya2704@gmail.com>
2022-08-22 17:14:08 +0300

    added rocks to some rooms

--- Rand_test/Networked_Prototype_0/Assets/_scripts/Dungeon/Inner_layout_manager.cs
+++ Rand_test/Networked_Prototype_0/Assets/_scripts/Dungeon/Inner_layout_manager.cs
@@ -7,7 +7,11 @@ using Unity.Netcode;
 //my chunk represents my chunk coordinates (it is 3*5) my grid represents my grid coordinates' world coordinate values
 //they are not exactly world coordinates but carry more information, if you recall in texture atlas each chunk was made up of
 //3x3 pixels, hence to get all the information needed a grid is needed 3*3 * 5*3 as you recall a 3x3 pixel is needed to convey
-//which direction a door is
+//which direction a door is. also our chunk system is 3*3 not 3*5 keep this in mind
+
+//ultimate comment = you see our canvas is 3*5 as it was when generatingthe world. since each of our chunks are made up
+//of 3*3 sections, and our grid is 3*3 * 5*3. chunk represents only 1 tile, while grid represents the entire canvas with
+//all its possible blocks. my chunks are my tiles
 
 public class rconfig
 {
@@ -31,7 +35,7 @@ public class Inner_layout_manager : NetworkBehaviour
     bool created;
     Dictionary<(int, int), bool> grid= new Dictionary<(int, int), bool> () ;
     public GameObject rock_prefab;
-
+    private System.Random rng = new System.Random();
 	private void Awake()
     {	
         created = false;
@@ -53,7 +57,7 @@ public class Inner_layout_manager : NetworkBehaviour
     void Start()
     {		
         // if (!IsServer) Destroy(gameObject);
-		inside = new Floor( 42 , rconfig.rx, rconfig.ry );
+		inside = new Floor( Random.Range(int.MinValue,int.MaxValue) , rconfig.rx, rconfig.ry );
         StartCoroutine(gen_layout());
         StartCoroutine(lay_out_layout());
        
@@ -99,6 +103,7 @@ public class Inner_layout_manager : NetworkBehaviour
         {
             for(int j = 0; j < rconfig.ry ; j++)
             {
+                //info of 9 pixels and it is called 15 times
 				add_section(i, j, inside.floor_data[(i, j)].value);
 			}	
 		}
@@ -111,7 +116,7 @@ public class Inner_layout_manager : NetworkBehaviour
 		{
 			for (int j = 0; j < rconfig.ry* 3; j++)
 			{				
-				if (grid[(i,j)]) //ch.Value
+				if (grid[(i,j)]) 
 				{
 					float tmp_x = i * grid_len_x/3  - room_len_x / 2;
 					float tmp_y = j * grid_len_y/3 - room_len_y / 2;
@@ -122,36 +127,73 @@ public class Inner_layout_manager : NetworkBehaviour
 		}
 	}
 
-    private void add_section(int xpos , int ypos, door_dir type  )
+    private void add_section(int xpos, int ypos, door_dir type)
     {
-		Dictionary<(int, int), bool> chunk = new Dictionary<(int, int), bool>();
-        for(int i = 0; i < 3; i++)
+        Dictionary<(int, int), bool> chunk = new Dictionary<(int, int), bool>();
+        for (int i = 0; i < 3; i++)
         {
-			for (int j = 0; j < 3; j++)
-			{
-				chunk[(i, j)] = false;
-			}
-		}
-		if ( ! inside.up_connection.Contains(type))
+            for (int j = 0; j < 3; j++)
+            {
+                chunk[(i, j)] = false;
+            }
+        }
+
+        float NW_prob = 100;
+        float NE_prob = 100;
+        float SE_prob = 100; 
+        float SW_prob = 100;
+			
+		if (!inside.up_connection.Contains(type))
         {
-			chunk[(1, 2)] = true;
-		}
-		if (! inside.down_connection.Contains(type))
-		{
-			chunk[(1, 0)] = true;
-		}
-		if (! inside.left_connection.Contains(type))
-		{
-			chunk[(0, 1)] = true;
-		}
-		if ( ! inside.right_connection.Contains(type))
-		{
-			chunk[(2, 1)] = true;
-		}
-		foreach(var ch in chunk)
+            chunk[(1, 2)] = true;
+        }
+        else
         {
-			grid[ ( 3 * xpos + ch.Key.Item1, 3 * ypos + ch.Key.Item2)  ] = true;
+            //if there is a connection to the upper room, the chances of upper right and upper left of the room
+            //having a boulder is lowered same with others
+			NW_prob -= 40 ;
+			NE_prob -= 40 ;
 		}
+		
+        if (!inside.down_connection.Contains(type))
+        {
+            chunk[(1, 0)] = true;
+        }else
+        {
+			SW_prob -= 40;
+			SE_prob -= 40;
+		}
+		if (!inside.left_connection.Contains(type))
+        {
+            chunk[(0, 1)] = true;
+        }
+	    else
+        {
+			NW_prob -= 40;
+			SW_prob -= 40;
+		}
+        if (!inside.right_connection.Contains(type))
+        {
+            chunk[(2, 1)] = true;
+        }
+		else
+		{
+			NE_prob -= 40;
+			SE_prob -= 40;
+		}
+
+		chunk[(0, 0)] =  rng.Next(0,101)  >= NW_prob ?true :false  ;
+        chunk[(2, 0)] =  rng.Next(0, 101) >= NE_prob ?true :false  ;
+        chunk[(0, 2)] =  rng.Next(0, 101) >= SE_prob ?true :false  ;
+        chunk[(2, 2)] =  rng.Next(0, 101) >= SW_prob ?true :false  ;
+	
+
+		foreach (var ch in chunk)
+        {
+            if (ch.Value) {
+                grid[(3 * xpos + ch.Key.Item1, 3 * ypos + ch.Key.Item2)] = true;
+            }
+        }
 	}
 }
 
