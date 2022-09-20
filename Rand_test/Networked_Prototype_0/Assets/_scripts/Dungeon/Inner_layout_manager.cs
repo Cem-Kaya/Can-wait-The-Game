aa79250d174bc6cdff16a6fd00970747774a47a4
BahaMert <bersoy@sabanciuniv.edu>
2022-09-20 15:55:36 +0300

    bugfix

--- Rand_test/Networked_Prototype_0/Assets/_scripts/Dungeon/Inner_layout_manager.cs
+++ Rand_test/Networked_Prototype_0/Assets/_scripts/Dungeon/Inner_layout_manager.cs
@@ -130,7 +130,9 @@ public class Inner_layout_manager : NetworkBehaviour
 				}
 				else if (room_type == "bossroom")
 				{
-					if (Dungeon_controller.instance.cleaned.ContainsKey((tmp_inf.x, tmp_inf.y))) {
+                   
+
+                    if (Dungeon_controller.instance.cleaned.ContainsKey((tmp_inf.x, tmp_inf.y))) {
 						GameObject win_door = Instantiate(win_door_prefab, new Vector3(0, 0, 0), Quaternion.identity);
 						win_door.GetComponent<NetworkObject>().Spawn();
 						spawned_objects.Add(win_door);
@@ -140,7 +142,8 @@ public class Inner_layout_manager : NetworkBehaviour
 						GameObject boss = Instantiate(boss_prefab, new Vector3(0, 0, 0), Quaternion.identity);
 						boss.GetComponent<NetworkObject>().Spawn();
 						spawned_objects.Add(boss);
-					}
+                        Dungeon_controller.instance.cleaned[(tmp_inf.x, tmp_inf.y)] = true; // change later if added teleport 
+                    }
 				}
 			}
 		}
